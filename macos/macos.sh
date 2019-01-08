d65d9942af934a475608451937236534a66f70a1
aggresss <aggresss@163.com>
2019-01-08 11:24:42 +0800

    2019-01-08 11:24:41 CST W01D2

--- macos/macos.sh
+++ macos/macos.sh
@@ -1,7 +1,63 @@
 # install command
 brew install \
-    gsed
+    autoconf \
+    automake \
+    libtool \
+    pkg-config \
+    autossh \
+    bash \
+    cmake \
+    cscope \
+    ctags \
+    doxygen \
+    emacs \
+    ffmpeg \
+    fontconfig \
+    freetype \
+    gcc@7 \
+    gd \
+    gettext \
+    git \
+    gnu-sed \
+    gnutls \
+    graphviz \
+    hexedit \
+    htop \
+    jq \
+    lua \
+    md5sha1sum \
+    minicom \
+    mosh \
+    openssl \
+    p7zip \
+    pandoc \
+    protobuf \
+    pstree \
+    python \
+    python@2 \
+    qt \
+    readline \
+    ruby \
+    socat \
+    sqlite \
+    telnet \
+    tmux \
+    tree \
+    unrar \
+    unzip \
+    wget \
+    wine \
+    xz
 
 brew cask install \
     vagrant \
-    xquartz
+    docker \
+    xquartz \
+    diffmerge \
+    emacs \
+    mactex \
+    qt-creator \
+    skim \
+    typora \
+    wireshark
+
