8560c990ccc674dd5be58dadfb253edf0c358b90
aggresss <aggresss@163.com>
2019-01-12 23:21:38 +0800

    2019-01-12 23:21:38 CST W01D6

--- ubuntu/bionic.sh
+++ ubuntu/bionic.sh
@@ -105,6 +105,16 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y code
 
+# virtualbox
+# https://www.virtualbox.org/wiki/Linux_Downloads
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian \
+    $(lsb_release -cs) \
+    contrib"
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-6.0
 
 #########################
 # configuration
