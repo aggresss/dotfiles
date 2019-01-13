393636df7b6be21c64a65c2480f6dc99f568f921
aggresss <aggresss@163.com>
2019-01-13 08:21:57 +0800

    2019-01-13 08:21:57 CST W01D7

--- ubuntu/bionic.sh
+++ ubuntu/bionic.sh
@@ -116,6 +116,17 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 sudo apt-get update
 sudo apt-get install virtualbox-6.0
 
+# vagrant
+# https://www.vagrantup.com/downloads.html
+VAGRANT_VERSION="2.2.3"
+wget -P ${HOME}/Downloads tmp https://releases.hashicorp.com/vagrant/2.2.3/vagrant_${VAGRANT_VERSION}_x86_64.deb
+sudo dpkg -i ${HOME}/Downloads/vagrant_${VAGRANT_VERSION}_x86_64.deb
+if [ $? -ne 0 ]; then
+    sudo apt-get -f -y install
+    sudo dpkg -i ${HOME}/Downloads/vagrant_${VAGRANT_VERSION}_x86_64.deb
+fi
+
+
 #########################
 # configuration
 #########################
