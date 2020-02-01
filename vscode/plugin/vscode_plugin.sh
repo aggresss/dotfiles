be74ca661d23f5c462327424883d221d91d9fd5f
aggresss <aggresss@163.com>
2020-02-01 10:25:35 +0800

    2020-02-01 10:25:35 CST W04D6

--- vscode/plugin/vscode_plugin.sh
+++ vscode/plugin/vscode_plugin.sh
@@ -1,2 +1,28 @@
+# Visual Studio Code plugin install list
+# code --install-extension
+# ext install
 
-ext install EditorConfig
+# vscode framework
+visualstudioexptteam.vscodeintellicode
+vscode-icons-team.vscode-icons
+# c/cpp
+ms-vscode.cpptools
+ms-vscode.cpptools-themes
+# golang
+ms-vscode.go
+# python
+ms-python.python
+# cmake
+twxs.cmake
+ms-vscode.cmake-tools
+# web debug
+msjsdiag.debugger-for-chrome
+ritwickdey.liveserver
+techer.open-in-browser
+# markdown
+yzhang.markdown-all-in-one
+# git
+donjayamanne.githistory
+# docker
+ms-azuretools.vscode-docker
+
