463e031988bc987bb385bdb26e285bed3edeba06
aggresss <aggresss@163.com>
2020-02-18 13:45:11 +0800

    2020-02-18 13:45:11 CST W07D2

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 
 set-alias vim "${env:ProgramFiles(x86)}\Vim\vim82\vim.exe"
 set-alias code "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\Code.exe"
+
+Function go_pwd
+{
+    $env:GOPATH="${PWD}"
+}
+
