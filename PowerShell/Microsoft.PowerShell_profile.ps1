2d9040584731519ec3dd5b0c4c5ad93e86686076
aggresss <aggresss@163.com>
2020-02-19 15:40:58 +0800

    2020-02-19 15:40:57 CST W07D3

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
 
+
+
 set-alias vim "${env:ProgramFiles(x86)}\Vim\vim82\vim.exe"
 set-alias code "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\Code.exe"
 
-Function go_pwd
-{
+function go_pwd {
     $env:GOPATH="${PWD}"
 }
 
+function s {set-location ${env:USERPROFILE}\workspace-scratch}
