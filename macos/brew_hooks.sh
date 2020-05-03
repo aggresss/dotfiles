8fa8811d315b5c115986d55db211ded89a613de0
aggresss <aggresss@163.com>
2020-05-03 16:35:54 +0800

    2020-05-03 16:35:54 CST W17D7

--- macos/brew_hooks.sh
+++ macos/brew_hooks.sh
@@ -8,4 +8,6 @@ ln -sf $(brew --prefix llvm)/bin/mosquitto_pub ${HOME}/bin/mosquitto_pub
 ln -sf $(brew --prefix llvm)/bin/mosquitto_sub ${HOME}/bin/mosquitto_sub
 ln -sf $(brew --prefix llvm)/bin/mosquitto_passwd ${HOME}/bin/mosquitto_passwd
 ln -sf $(brew --prefix llvm)/bin/mosquitto_rr ${HOME}/bin/mosquitto_rr
+## curl
+ln -sf $(brew --prefix curl)/bin/curl ${HOME}/bin/curl
 
