0de0f2261d0d8c8f45518735e8a3c87d48d37781
aggresss <aggresss@163.com>
2020-08-20 20:25:00 +0800

    2020-08-20 20:25:00 +08 W33D4

--- windows/windows.ps1
+++ windows/windows.ps1
@@ -8,6 +8,10 @@ winget install node.js
 winget install Chrome
 winget install Firefox
 winget install "Microsoft Edge"
+winget install "VLC media player"
+winget install Wireshark
 winget install "Visual Studio Code"
 winget install "Visual Studio Community"
+winget install GPU-Z
+winget install CPU-Z
 
