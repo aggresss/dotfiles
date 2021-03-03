cb74f39e8e43a7bbe903b42a3081a82d575b00d0
aggresss <aggresss@163.com>
2021-03-03 19:19:02 +0800

    2021-03-03 19:19:02 +08 W9D3

--- windows/windows.ps1
+++ windows/windows.ps1
@@ -1,4 +1,4 @@
-# install list
+# Install list
 
 winget install Microsoft.PowerToys
 winget install Git.Git
@@ -20,3 +20,29 @@ winget install Microsoft.VisualStudioCode-User-x64
 winget install Microsoft.VisualStudio.Community
 winget install CPUID.GPU-Z
 winget install TechPowerUpCPU-Z
+
+# Environment management
+
+## Perl
+$perl_path = "${Env:HOMEDRIVE}\Strawberry\perl\bin"
+if ($(Test-Path $perl_path)) {
+  env_append PATH (Resolve-Path $perl_path)[-1] User
+}
+
+## Ruby
+$ruby_path = "${Env:HOMEDRIVE}\Ruby*\bin"
+if ($(Test-Path $ruby_path)) {
+  env_append PATH (Resolve-Path $ruby_path)[-1] User
+}
+
+## Lua
+$lua_path = "${Env:USERPROFILE}\lua"
+if ($(Test-Path $lua_path)) {
+  env_append PATH (Get-ChildItem $lua_path)[-1].FullName User
+}
+
+## Maven
+$maven_path = "${Env:USERPROFILE}\maven\*\bin"
+if ($(Test-Path $maven_path)) {
+  env_append PATH (Resolve-Path $maven_path)[-1] User
+}
