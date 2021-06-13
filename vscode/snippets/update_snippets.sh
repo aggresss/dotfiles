eb798a85aec85622dfd243b59f1007ef5ddd0a81
aggresss <aggresss@163.com>
2021-06-13 21:38:55 +0800

    2021-06-13 21:38:55 CST W24D0

---
+++ vscode/snippets/update_snippets.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+case $(uname) in
+    Darwin)
+        GLOBAL_WORKSPACE="${HOME}/Library/Application Support/Code/User"
+        ;;
+    Linux)
+        GLOBAL_WORKSPACE="${HOME}/.config/Code/User"
+        ;;
+    MINGW*)
+        GLOBAL_WORKSPACE="%APPDATA%\Code\User"
+        ;;
+esac
+
+set -v
+cp ${PWD}/go.json "${GLOBAL_WORKSPACE}/snippets/"
+set +v
