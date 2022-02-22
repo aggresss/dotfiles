1b87bb922fd235d1460210c6f0fa88d221bede55
aggresss <aggresss@163.com>
2022-02-22 09:25:36 +0800

    2022-02-22 09:25:36 CST W08D2

--- npm/npm_install.sh
+++ npm/npm_install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-npm config set registry https://registry.npm.taobao.org
+npm config set registry https://registry.npmmirror.com
 
 npm install -g \
+    npm \
+    yarn \
+    typescript \
     serve \
     create-react-app
