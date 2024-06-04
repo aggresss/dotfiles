9a12c44205c2acea3b388be3914c76f13bea18f3
aggresss <aggresss@163.com>
2024-06-04 17:41:16 +0800

    2024-06-04 17:41:16 CST W22D2

--- ubuntu/ubuntu_basic.sh
+++ ubuntu/ubuntu_basic.sh
@@ -73,6 +73,7 @@ sudo apt-get update && sudo apt-get install -y \
     gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad \
     cpu-checker \
     htop \
+    iftop \
     nmon \
     bmon \
     sysstat \
